c1e31719d5c8f693d3750597b427c6a182e03406
桐谷翔 <lionactheart007@gmail.com>
2025-09-19 11:59:28 +0900

    更新

---
+++ Assets/Scripts/DrinkData.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class DrinkData : MonoBehaviour
+{
+    Rigidbody2D rbody;
+    public int itemNum;
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.bodyType = RigidbodyType2D.Static;
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //PlayerのHPが最大なら何もしない
+            if (GameManager.playerHP < 3)
+            {
+                GameManager.playerHP++ ;
+            }
+
+            //該当する識別番号を取得済み
+            GameManager.itemPickedState[itemNum] = true ;
+
+            //アイテム取得の演出
+            GetComponent<CircleCollider2D>().enabled = false ;
+            rbody.bodyType = RigidbodyType2D.Dynamic;
+            rbody.AddForce(new Vector2(0, 5),ForceMode2D.Impulse);
+            Destroy(gameObject,0.5f);
+        }
+    }
+}
